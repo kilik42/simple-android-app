ea621c05e2ffe42b367c2c070b5065fad587904b
marvin evins <maevins@cps.edu>
2016-06-29 20:29:32 -0500

    numbers
    work with numbers java

---
+++ app/src/main/java/Numbers.java
@@ -0,0 +1,46 @@
+/**
+ * Created by marvinevins on 6/29/16.
+ */
+public class Numbers {
+
+    public static void main(String[] args) {
+        int age = 25;
+        int myFavoriteSports = -50;
+       // float age1 = (float) 25.5;
+        int age2 = 25;
+
+        int num1 = 10;
+        int num2 = 12;
+
+        int sum = age + myFavoriteSports ;
+        System.out.println(sum);
+        System.out.println(num1 + num2);
+
+        int difference = 100 -20;
+
+        int product = 22 *5;
+        int divide = 15/5;
+
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(divide);
+
+
+
+        int modulo = 20/3;
+        System.out.println(modulo);
+
+        //32 bits of storage
+        float bankBalance =  432.23f;
+        //64 bits of storage
+        double  thisIsADouble = 5678.98676;
+
+        // for big big big whole numbers(up to 2 billion)
+        long donaldTrumpsBankAccount;
+
+        Integer myInt = 550;
+
+        String intStr = myInt.toString();
+
+    }
+}
